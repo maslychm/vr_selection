612ab6f2a871b5170c5a5aae3f6c64c075fb35e1
Mykola <maslychm@gmail.com>
2022-06-16 11:32:50 -0400

    tag typo fix, right hand visual

--- Assets/Scripts/Demo/DestroyParentOnPlayerHit.cs
+++ Assets/Scripts/Demo/DestroyParentOnPlayerHit.cs
@@ -1,52 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider), typeof(Rigidbody))]
 public class DestroyParentOnPlayerHit : MonoBehaviour
 {
-    
-    private void DestroyEnemyParent()
+    private void DestroyParentObject()
     {
         Destroy(gameObject.transform.parent.gameObject);
     }
 
     public void OnTriggerEnter(Collider other)
     {
+        //print($"{tag} hit by {other.tag}");
 
-        // string otherTag = "as";
-        // if (otherTag.Lower().Contains())
+        if (gameObject.tag.Contains(other.tag))
+        {
+            DestroyParentObject();
+            return;
+        }
 
-        print($"collided with: {other.tag}");
-
-        switch (other.tag)
-        { 
-            case "Player":
-                DestroyEnemyParent();
-                break;
-
-            case "cube":
-                print("RUN");
-                if (gameObject.tag.Contains("cube"))
-                    DestroyEnemyParent();
-                break;
-
-            case "star":
-                if (gameObject.tag.Contains("star"))
-                    DestroyEnemyParent();
-                break;
-
-            case "pyramid":
-                if (gameObject.tag.Contains("pyramid"))
-                    DestroyEnemyParent();
-                break;
-
-            case "sphere":
-                if (gameObject.tag.Contains("shpere"))
-                    DestroyEnemyParent();
-                break;
-            
+        if (other.tag == "Player")
+        {
+            DestroyParentObject();
+            return;
         }
     }
-
 }

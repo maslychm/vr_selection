680f9c870add923f7b433bed2b746b14c951a724
Yohan Hmaiti <yohanhmaiti@knights.ucf.edu>
2022-07-04 22:28:29 -0400

    updated the interactable files (needs testing)

--- Assets/Scripts/Selection/XRGestureInteractable.cs
+++ Assets/Scripts/Selection/XRGestureInteractable.cs
@@ -7,7 +7,10 @@ public class XRGestureInteractable : MonoBehaviour
     private XRGestureFilterInteractor gestureInteractor;
 
     // grid selector var to be used instead of gesture interactor when needed
-    private XRGridSelectorInteractor secondInteractorHelper; 
+    private XRGridSelectorInteractor secondInteractorHelper;
+
+    // add a third access helper from the MiniMap Interactor 
+    private MiniMapInteractor thirdInteractorHelper;
     
     [SerializeField] private Material hoverMaterial;
 
@@ -16,19 +19,62 @@ public class XRGestureInteractable : MonoBehaviour
 
     public bool debug = false;
 
+    private List<Component> listOfFiles_Components_ToBe_Used;
+
+    private Component chosenComponentToBeUsedNow;
+
     private void Awake()
     {
+        // store the whole components in one list for ease of use and dynamic access/switch 
+        listOfFiles_Components_ToBe_Used = new List<Component>();
+        listOfFiles_Components_ToBe_Used.Add(gestureInteractor);
+        listOfFiles_Components_ToBe_Used.Add(secondInteractorHelper);
+        listOfFiles_Components_ToBe_Used.Add(thirdInteractorHelper);
+
+
         meshRenderers = new List<MeshRenderer>(GetComponents<MeshRenderer>());
         if (meshRenderers.Count == 0)
             meshRenderers = new List<MeshRenderer>(GetComponentsInChildren<MeshRenderer>());
         defaultMaterial = meshRenderers[0].material;
 
-        if (secondInteractorHelper == null)
+        if (secondInteractorHelper == null && thirdInteractorHelper == null)
             gestureInteractor = FindObjectOfType<XRGestureFilterInteractor>();
 
         // added to override the inability to access components through the gesture Interactor 
-            if (gestureInteractor == null)
-                secondInteractorHelper = FindObjectOfType<XRGridSelectorInteractor>();
+        if (gestureInteractor == null && thirdInteractorHelper == null)
+            secondInteractorHelper = FindObjectOfType<XRGridSelectorInteractor>();
+
+        // new
+        if (thirdInteractorHelper != null)
+            thirdInteractorHelper  =  FindObjectOfType<MiniMapInteractor>();
+
+
+        // iterate through all the possible options to call the functions later 
+        // Suggestion ---------------------------------------------TO BE CHECKED---------------------------------------------
+        // ------------------------------------------------------------------------------------------------------------------
+        // ------------------------------------------------------------------------------------------------------------------
+        // ------------------------------------------------------------------------------------------------------------------
+            /* for(int i = 0; i < listOfFiles_Components_ToBe_Used.Count; i++)
+             {
+                 if (listOfFiles_Components_ToBe_Used[i] == gestureInteractor && listOfFiles_Components_ToBe_Used[i] != null)
+                 {
+                     gestureInteractor =  FindObjectOfType<XRGestureFilterInteractor>();
+                     listOfFiles_Components_ToBe_Used[i] =  FindObjectOfType<XRGestureFilterInteractor>();
+                     chosenComponentToBeUsedNow = gestureInteractor;
+                 }
+                 else if (listOfFiles_Components_ToBe_Used[i] == secondInteractorHelper && listOfFiles_Components_ToBe_Used[i] != null)
+                 {
+                     secondInteractorHelper = FindObjectOfType<XRGridSelectorInteractor>();
+                     listOfFiles_Components_ToBe_Used[i] =  FindObjectOfType<XRGridSelectorInteractor>();
+                     chosenComponentToBeUsedNow = secondInteractorHelper;
+                 }
+                 else if (listOfFiles_Components_ToBe_Used[i] == thirdInteractorHelper && listOfFiles_Components_ToBe_Used[i] != null)
+                 {
+                     thirdInteractorHelper = FindObjectOfType<MiniMapInteractor>();
+                     listOfFiles_Components_ToBe_Used[i] =  FindObjectOfType<MiniMapInteractor>();
+                     chosenComponentToBeUsedNow = thirdInteractorHelper;
+                 }
+             }*/
     }
 
     // add a getter for the meshrenderer list 
@@ -61,14 +107,20 @@ public class XRGestureInteractable : MonoBehaviour
         foreach (var mr in meshRenderers)
             mr.material = hoverMaterial;
 
-        // check which one is ready to be used 
-        if (gestureInteractor != null)
+        /* for(int i = 0; i < listOfFiles_Components_ToBe_Used.Count; i++)
+             if (listOfFiles_Components_ToBe_Used[i] != null)
+                 listOfFiles_Components_ToBe_Used[i].AddtoHighlighted(gameObject);
 
+         */
+        if (secondInteractorHelper == null && thirdInteractorHelper == null)
             gestureInteractor.AddtoHighlighted(gameObject);
 
-        else
-
+        if (gestureInteractor == null && thirdInteractorHelper == null)
             secondInteractorHelper.AddtoHighlighted(gameObject);
+
+        if (thirdInteractorHelper != null)
+            thirdInteractorHelper.AddtoHighlighted(gameObject);
+
     }
 
     private void EndHover()
@@ -78,14 +130,17 @@ public class XRGestureInteractable : MonoBehaviour
         foreach (var mr in meshRenderers)
             mr.material = defaultMaterial;
 
-        // check which one is ready to be used 
-        if(gestureInteractor != null)
 
+        if (secondInteractorHelper == null && thirdInteractorHelper == null)
             gestureInteractor.RemoveFromHighlighted(gameObject);
 
-        else
-
+        if (gestureInteractor == null && thirdInteractorHelper == null)
             secondInteractorHelper.RemoveFromHighlighted(gameObject);
+
+        if (thirdInteractorHelper != null)
+            thirdInteractorHelper.RemoveFromHighlighted(gameObject);
+
+
     }
 
     private void dprint(string msg)

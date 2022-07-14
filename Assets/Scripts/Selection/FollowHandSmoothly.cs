066e36e99e7d3dfc181b3698e566c4110d7f7798
Mykola <maslychm@gmail.com>
2022-07-14 15:50:40 -0400

    Use lerping for smoothing

--- Assets/Scripts/Selection/FollowHandSmoothly.cs
+++ Assets/Scripts/Selection/FollowHandSmoothly.cs
@@ -12,7 +12,8 @@ public class FollowHandSmoothly : MonoBehaviour
     [SerializeField] private bool applyFilter = true;
     [SerializeField] private bool isChildOfTracked = true;
 
-    private Vector3 lastFilteredPosition, lastFilteredRotation;
+    private Vector3 lastFilteredPosition;
+    private Quaternion lastFilteredQuaternion;
 
     private void Start()
     {
@@ -39,84 +40,18 @@ public class FollowHandSmoothly : MonoBehaviour
 
     private void SmoothingStepMethodAsChildOfTracked()
     {
-        // Saves vectors for positions externally instead of relying on own transform.position for getting last position
-        // The suspicion is that Unity updates the transforms and positions of children before the update is called -> this is true
-
-        transform.position = handToFollow.position * newValConfidence + lastFilteredPosition * (1 - newValConfidence);
+        transform.position = Vector3.Lerp(lastFilteredPosition, handToFollow.position, newValConfidence);
         lastFilteredPosition = transform.position;
 
-        // Problem happens when we go around the 0, because then Unity automatically mods by 360
-        // If the difference between old value and new value along any of the dimensions is > 180, we need to set the old value into the "opposite" rotation
-        // before applying the filter
-
-        Vector3 diff = lastFilteredRotation - handToFollow.eulerAngles;
-        Vector3 oldFixedEulerAngles = lastFilteredRotation;
-
-        if (diff.x < -180)
-        {
-            oldFixedEulerAngles.x += 360;
-        }
-        else if (diff.x > 180)
-        {
-            oldFixedEulerAngles.x -= 360;
-        }
-
-        if (diff.y < -180)
-        {
-            oldFixedEulerAngles.y += 360;
-        }
-        else if (diff.y > 180)
-        {
-            oldFixedEulerAngles.y -= 360;
-        }
-
-        if (diff.z < -180)
-        {
-            oldFixedEulerAngles.z += 360;
-        }
-        else if (diff.z > 180)
-        {
-            oldFixedEulerAngles.z -= 360;
-        }
-
-        transform.eulerAngles = handToFollow.eulerAngles * newValConfidence + oldFixedEulerAngles * (1 - newValConfidence);
-        lastFilteredRotation = transform.eulerAngles;
+        transform.rotation = Quaternion.Lerp(lastFilteredQuaternion, handToFollow.rotation, newValConfidence);
+        lastFilteredQuaternion = transform.rotation;
     }
 
     private void SmoothingStepMethodAsChildOfNotTracked()
     {
-        transform.position = handToFollow.position * newValConfidence + transform.position * (1 - newValConfidence);
-
-        Vector3 diff = transform.eulerAngles - handToFollow.eulerAngles;
-        Vector3 oldNewEulerAngles = transform.eulerAngles;
-
-        if (diff.x < -180)
-        {
-            oldNewEulerAngles.x += 360;
-        }
-        else if (diff.x > 180)
-        {
-            oldNewEulerAngles.x -= 360;
-        }
-
-        if (diff.y < -180)
-        {
-            oldNewEulerAngles.y += 360;
-        }
-        else if (diff.y > 180)
-        {
-            oldNewEulerAngles.y -= 360;
-        }
-
-        if (diff.z < -180)
-        {
-            oldNewEulerAngles.z += 360;
-        }
-        else if (diff.z > 180)
-        {
-            oldNewEulerAngles.z -= 360;
-        }
-
-        transform.eulerAngles = handToFollow.eulerAngles * newValConfidence + oldNewEulerAngles * (1 - newValConfidence);
+        transform.SetPositionAndRotation(
+            Vector3.Lerp(transform.position, handToFollow.position, newValConfidence),
+            Quaternion.Lerp(transform.rotation, handToFollow.rotation, newValConfidence)
+            );
     }
 }

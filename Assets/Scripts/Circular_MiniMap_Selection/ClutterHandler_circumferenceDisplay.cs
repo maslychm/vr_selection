baf27fe7a95d0a9341c206a62a5a8c959b9ddd8a
unknown <yohanhmaiti@knights.ucf.edu>
2022-07-28 09:55:54 -0400

    Fixed the distancing/grabbing needs slight changes now/maybe fix orientation and make minimap smaller

--- Assets/Scripts/Circular_MiniMap_Selection/ClutterHandler_circumferenceDisplay.cs
+++ Assets/Scripts/Circular_MiniMap_Selection/ClutterHandler_circumferenceDisplay.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 public class ClutterHandler_circumferenceDisplay : MonoBehaviour
 {
     [SerializeField] public float radius = 0.015f;
-    [SerializeField] public float offset = 0f;
+    [SerializeField] public float offset;
     private int totalObjectsCount = 8;
     private Transform centreCircleTransform;
     private static Dictionary<GameObject, bool> spotsAroundMiniMap;
@@ -68,16 +68,6 @@ public class ClutterHandler_circumferenceDisplay : MonoBehaviour
         if (await == true)
             return;
 
-        // get all the colliders with the hand itself and store them in array
-        // ame as the radius of the rioght hand's sphere
-        //_collidersWithHand = Physics.OverlapSphere(TheHand.transform.position, 0.03f);
-
-        ////if(await == false)
-        ////    makeSpotsReady();
-        //// we only fill the spots if there is clutter
-        //if (_collidersWithHand.Length <= 1)
-        //    return;
-
         // just as a safety check we will clear the lists here again
         if (collidingWithHandDuplicates.Count > 0)
         {
@@ -88,23 +78,6 @@ public class ClutterHandler_circumferenceDisplay : MonoBehaviour
         {
             spotsAroundMiniMap[key] = false;
         }
-
-        //// get all the colliders with the hand itself and store them in array
-        //// ame as the radius of the rioght hand's sphere
-        //_collidersWithHand = Physics.OverlapSphere(TheHand.transform.position, 0.03f);
-
-        ////if(await == false)
-        ////    makeSpotsReady();
-        //// we only fill the spots if there is clutter
-        //if (_collidersWithHand.Length <= 1)
-        //    return;
-
-        // print all the colliders currently saved
-        //foreach (var currentlyTouching in _collidersWithHand)
-        //{
-        //    if (currentlyTouching.gameObject.name.Contains("phere"))
-        //        Debug.Log("colliding with -> " + currentlyTouching);
-        //}
 
         // check if the user clicked the trigger
         if (clickedRightHandController.action.WasPressedThisFrame())
@@ -170,10 +143,10 @@ public class ClutterHandler_circumferenceDisplay : MonoBehaviour
             Vector3 newPosition = new Vector3(horizental_placement, vertical_placement, 0);
 
             // now let's get the final position around the circle for the current iteration
-            var extendedPosition = newPosition * (radius + offset) + centreCircleTransform.localPosition;
 
-            // let's create the placeHolder/empty gameObject now in the environment through instantiation
-            //var tempPlaceHolder = Instantiate(new GameObject(), extendedPosition, Quaternion.identity) as GameObject;
+            // ITS 0.3F TO GET THE RIGHT CIRCUMFERENCE POSITIONING 
+            // MOVE BY 0.1 DEPENDING ON THE RESIZING OF THE MINIMAP
+            var extendedPosition = newPosition * (radius - 0.3f) + centreCircleTransform.localPosition;
 
             // checkpoint 1
             var tempPlaceHolder = Instantiate(new GameObject(), extendedPosition, Quaternion.identity) as GameObject;
@@ -184,65 +157,6 @@ public class ClutterHandler_circumferenceDisplay : MonoBehaviour
             spotsAroundMiniMap.Add(tempPlaceHolder, false);
         }
     }
-
-    // this function should take care of all the duplication process and fill the zones :)
-    // In this process we assume that we are only dealing with spheres
-    /* private void duplicateCurrentColliders()
-     {
-         List<XRGestureInteractable> originalInteractables = FindObjectsOfType<XRGestureInteractable>().ToList();
-         // isTrigger?????
-         foreach (var currentCollider in _collidersWithHand)
-         {
-             // check for anythin g that is nbot a sphere
-             if (!currentCollider.gameObject.name.Contains("phere") )
-             {
-                 continue;
-             }
-
-             // store the game Object
-             GameObject currentCollidingObjWithHand = currentCollider.gameObject;
-
-             if (duplicatedBefore.Count != 0 && duplicatedBefore.Contains(currentCollidingObjWithHand))
-                 continue;
-             // duplicate this colliding gameObject
-             GameObject duplicate = Instantiate(currentCollidingObjWithHand);
-
-             // when we duplicate the object it will already have the outline component in it
-             // no need for extra component addition
-
-             // keep the same scale as the one shown in the Mini Map
-             duplicate.transform.localScale = new Vector3(0.07f, 0.07f, 0.07f);
-
-             // the object that is in the minimap will have these already deleted
-             // HOWEVER
-             // add sanity checks
-             if (duplicate.GetComponent<XRGestureInteractable>() != null)
-                 Destroy(duplicate.GetComponent<XRGestureInteractable>());
-             if (duplicate.GetComponent<Object_collected>() != null)
-                 Destroy(duplicate.GetComponent<Object_collected>());
-
-             // ADD shapeItem_3, Mykola asked for this, need to ask why again?????
-             // this technically only tackles a highlight display
-             duplicate.AddComponent<shapeItem_3>();
-             duplicate.GetComponent<shapeItem_3>().original = currentCollider.gameObject;
-
-             // no need to add a collider already present in original
-
-             // set kinematic and remove gravity
-             duplicate.GetComponent<Rigidbody>().isKinematic = true;
-             duplicate.GetComponent<Rigidbody>().useGravity = false;
-
-             duplicate.tag = "unclutterDuplicate";
-
-             // now simply add this duplicate to a list for access
-             collidingWithHandDuplicates.Add(currentCollidingObjWithHand, duplicate);
-
-             duplicatedBefore.Add(currentCollidingObjWithHand);
-
-             // set to false here, means when spawning it we need to set it to active again
-             duplicate.SetActive(false);
-         }
-     }*/
 
     /// <summary>
     /// this will only insert when the hand actually hits the items on the mini map
@@ -255,14 +169,6 @@ public class ClutterHandler_circumferenceDisplay : MonoBehaviour
 
     private void insertToSpots()
     {
-        // first if we do not have any items to insert
-        // if we do not have any spots to fill
-        // we simply return
-
-        //if (_collidersWithHand.Length < 2)
-        //{
-        //    return;
-        //}
 
         List<GameObject> temp = new List<GameObject>();
 
@@ -296,20 +202,17 @@ public class ClutterHandler_circumferenceDisplay : MonoBehaviour
             print("Started INSERTION ****");
             GameObject _NextAvailableSpot = availableindex;
 
-            print("MID INSERTION ****");
-            // initiate the insertion process
-            // update the position the rotation and the parent
 
             if (temp[i].gameObject.GetComponent<shapeItem_2>() == null)
                 continue;
+
             if (originalToDuplicate[temp[i].gameObject.GetComponent<shapeItem_2>()] == null)
                 print("Problem 2 triggered");
+
             print("HERE IS THE ORIGINAL TO DUPLICATE INFO ->>>>" + temp[i].gameObject.GetComponent<shapeItem_2>().gameObject.name + " " + originalToDuplicate[temp[i].gameObject.GetComponent<shapeItem_2>()].gameObject.name);
             originalToDuplicate[temp[i].gameObject.GetComponent<shapeItem_2>()].gameObject.transform.position = _NextAvailableSpot.transform.position;
 
             originalToDuplicate[temp[i].gameObject.GetComponent<shapeItem_2>()].gameObject.transform.rotation = _NextAvailableSpot.transform.rotation;
-
-            print("ARE WE HERE ???????????????????");
 
             // this should fix the need to rotate and position needs to be going along with the minimap/hand
             originalToDuplicate[temp[i].gameObject.GetComponent<shapeItem_2>()].gameObject.transform.SetParent(_NextAvailableSpot.transform);
@@ -319,26 +222,6 @@ public class ClutterHandler_circumferenceDisplay : MonoBehaviour
             print("COMPLETED INSERTION ****");
 
             spotsAroundMiniMap[availableindex] = true;
-
-            /*  if(originaltoduplicatewithgameObject.ContainsKey(_collidersWithHand[i].gameObject) == false)
-              {
-                  print("PROBLEM 2");
-                  continue;
-              }
-
-              originaltoduplicatewithgameObject[_collidersWithHand[i].gameObject].transform.position = _NextAvailableSpot.transform.position;
-              originaltoduplicatewithgameObject[_collidersWithHand[i].gameObject].transform.rotation = _NextAvailableSpot.transform.rotation;
-
-              print("ARE WE HERE ???????????????????");
-
-              // this should fix the need to rotate and position needs to be going along with the minimap/hand
-              originaltoduplicatewithgameObject[_collidersWithHand[i].gameObject].transform.SetParent(_NextAvailableSpot.transform);
-
-              collidingWithHandDuplicates.Add(_collidersWithHand[i].gameObject, originaltoduplicatewithgameObject[_collidersWithHand[i].gameObject]);
-
-              print("COMPLETED INSERTION ****");
-
-              spotsAroundMiniMap[availableindex] = true;*/
         }
     }
 }
